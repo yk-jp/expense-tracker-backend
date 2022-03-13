6ab5b9cd7263ea9c18b0180d4232ab3722935f83
yusuke <k82.y12@gmail.com>
2022-03-13 11:32:37 -0700

    create tables

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.db import models
 
 @api_view(['GET'])
 def getData(request):
     person = {"name": "users"}
     return Response(person)
+
+# @api_view(['POST'])
+# def postData(request):
+
+#     return Response()
--- core/models.py
+++ core/models.py
@@ -1,29 +1,74 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator 
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True)
+    email = models.EmailField(
+        max_length=50,
+        unique=True
+    )
     password = models.CharField(max_length=50)
 
     def __str__(self):
         return self.email
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    CATEGORY_CHOICES = (
+        ('Food', 'Food'),
+        ('Rent', 'Rent'),
+        ('Phone', 'Phone'),
+        ('Transportation', 'Transportation'),
+        ('Education', 'Education'),
+        ('Insurance', 'Insurance'),
+        ('Random', 'Random'),
+        ('Edit', 'Edit'),
+    )
+
+    name = models.CharField(
+        max_length=50,
+        choices=CATEGORY_CHOICES
+    )
 
     def __str__(self):
         return self.name
 
+
 class Budget(models.Model):
-    event = models.CharField(max_length=50)
-    memo = models.TextField(null=True, blank=True)
+    EVENT_CHOICES = (
+        ('Income', 'Income'),
+        ('Outcome', 'Outcome')
+    )
+
+    event = models.CharField(
+        max_length=50,
+        choices=EVENT_CHOICES
+    )
+    amount = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
+    memo = models.TextField(
+        null=True,
+        blank=True
+    )
     date = models.DateField(auto_now_add=True)
     # associations
-    user = models.ForeignKey(User, verbose_name=("user"), on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, verbose_name=("category"), null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(
+        User,
+        verbose_name=("user"),
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name=("category"),
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
-        return self.id + ' ' + self.event + ' ' + self.date
-
-
+        return str(self.user) + ' ' + str(self.event) + ' ' + str(self.category)
