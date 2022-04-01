6062849a4a78b3ef5f2f85ffae8a01bf1dd881d8
yusuke <k82.y12@gmail.com>
2022-03-31 23:41:33 -0700

    test api

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from core.models import User, Category, Transaction
 from core.serializers import UserSerializer, CategorySerializer, TransactionSerializer
@@ -8,11 +9,17 @@ from core.serializers import UserSerializer, CategorySerializer, TransactionSeri
 def getData(request):
     all_transactions_current_month = Transaction.objects.all()
 
-    serializer = TransactionSerializer(all_transactions_current_month, many=True)
+    serializer = TransactionSerializer(
+        all_transactions_current_month, many=True)
 
     return Response(serializer.data)
 
-# @api_view(['POST'])
-# def postData(request):
 
-#     return Response()
+@api_view(['POST'])
+def transaction_register(request):
+    print(request.data)
+    serializer = TransactionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
