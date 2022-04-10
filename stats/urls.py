5314a4241c2c80396a5e258696cd23b9fcf08337
yk-jp <k82.y12@gmail.com>
2022-04-10 15:35:53 -0700

    add function for fetching specified data by category

--- stats/urls.py
+++ stats/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_stats,name='stats')
+    path('', views.get_stats,name='stats'),
+    path('category/<str:name>/',views.get_category_stats, name='category')
 ]
--- stats/views.py
+++ stats/views.py
@@ -12,8 +12,6 @@ from decimal import Decimal
 
 @api_view(['GET'])
 def get_stats(request):
-    print("body: ", json.dumps(request.data, indent=4))  
-
     today = datetime.date.today()
     current_year = today.year
     current_month = today.month
@@ -54,6 +52,61 @@ def get_stats(request):
             } 
         
     print("get_stats: ", json.dumps(stats,indent=4))
+    
+    return Response({
+        "result": stats,
+        "message":"successfully fetched",
+        "is_success": True
+    })
+
+
+@api_view(['GET'])
+def get_category_stats(request, name):
+    print("params: ", json.dumps(name, indent=4))  
+
+    today = datetime.date.today()
+    current_year = today.year
+    current_month = today.month
+    this_month = get_start_end_of_month(current_year,current_month)
+        
+    
+    try:
+        transactions_current_month_category = Transaction.objects.filter(
+        t_date__range=(this_month["start"], this_month["end"]), category__name=name
+        ) 
+        transactions_current_month_category = TransactionSerializer(
+            transactions_current_month_category, many=True).data
+        
+    except Exception:
+        return Response(
+          {
+            "message":"Failed to fetch data. Please try to refresh the page",
+            "is_success":False
+          },
+          status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        ) 
+    
+    stats = {
+        "Income": 0,
+        "Expense": 0,
+        "Balance": 0
+    }
+    
+    for transaction in transactions_current_month_category:
+        if transaction["event"] == "Income":
+            stats = {
+                **stats,
+                "Income": Decimal(stats["Income"]) + Decimal(transaction["amount"]),
+                "Balance": Decimal(stats["Balance"]) + Decimal(transaction["amount"])
+            }
+        else:
+             stats = {
+                **stats,
+                "Expense": Decimal(stats["Expense"]) + Decimal(transaction["amount"]),
+                "Balance": Decimal(stats["Balance"]) - Decimal(transaction["amount"])
+            } 
+       
+    print("get_stats_category: ", json.dumps(stats,indent=4))
     
     return Response({
         "result": stats,
