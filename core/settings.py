78de85a897192f24e12a8769aa0254d0b82c0158
yk-jp <k82.y12@gmail.com>
2022-06-12 21:15:48 -0700

    fix redis coonnections

--- core/settings.py
+++ core/settings.py
@@ -173,7 +173,7 @@ CORS_ALLOWED_ORIGINS = [
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379'
+        'LOCATION': env('REDIS_HOST') 
     },
 }
 
