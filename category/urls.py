f2f8ae7b2486de8155fcdae073c2e3243258f458
yk-jp <k82.y12@gmail.com>
2022-09-10 21:04:31 -0700

    fix endpoint

--- category/urls.py
+++ category/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('save',views.add_category, name='add_category'),
-    path('update/<int:id>/',views.update_category, name='update_category'),
-    path('delete/<int:id>/',views.delete_category, name='delete_category'),
+    path('update/<str:type>/<int:id>/',views.update_category, name='update_category'),
+    path('delete/<str:type>/<int:id>/',views.delete_category, name='delete_category'),
     path('<str:type>', views.get_category_all, name='category'),
 ]
 
